4fddc85fd7b3e7ea6e7aa4e642735bf2b7663fd8
Roman <romko43@gmail.com>
2015-12-15 21:58:50 +0000

    change script files. add scripts for getting data from api weather

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(function() {
+    console.log( "ready!" );
+
+// forecast data
+    $.getJSON('http://api.openweathermap.org/data/2.5/forecast/daily?q='+
+             'Newark'+
+             '&units=metric&cnt=7' + '&lang=' + 'en' + '&callback=?' + '&appid=2239135b18e8b5e093a144e55b94b5d6',
+            function(data){
+                $('#forecast-date').html('<div class="day-active">' + data.list[0].dt + '</div>' +
+                                        '<div class="data-active">' + data.list[0].dt + '</div>');
+                $('#forecast-img').html('<img src="img/w/' + data.list[0].weather[0].icon + '.png" alt="weather icon" />' + 
+                                        '<p>' + data.list[0].weather[0].description + '</p>');
+                $('#location-box').html('<p id="location">' + data.city.name + ', ' + data.city.country + '</p>');
+                //$('#temptoday').html(data.list[0].temp.day);
+                //$('#temptomorrow').html(data.list[1].temp.day);
+                //$('#tempaftertomorrow').html(data.list[2].temp.day);
+            });
+
+
+
+// buttons in chart-box. switch chart in box
+    $('.button-box').on('click', 'a', function( event ) {
+         event.preventDefault();
+         var parrent = $(this).parent(),
+             name = parrent.data('name');
+         parrent.addClass('active').siblings('li').removeClass('active');
+         parrent.closest('.forecast-box-middle').find('.chart-box-item.'+name).addClass('active').siblings('.chart-box-item').removeClass('active');
+        
+    });
+// slider. move body whith slider
+    $('#aside').mouseenter(function() {
+        $(this).closest('body').addClass('open');
+    });
+    $('#aside').mouseleave(function() {
+        $(this).closest('body').removeClass('open');
+    });
+
+
+
+});
