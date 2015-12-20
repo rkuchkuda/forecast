60f293968bdacd368c586fa57a3fee1c16653ce0
Roman <romko43@gmail.com>
2015-12-20 00:32:32 +0000

    add scripts for search by location

--- js/index.js
+++ js/index.js
@@ -2,6 +2,25 @@
 $( document ).ready(function() {
     console.log( "ready!" );
     
+    $('#btn').click(function () {
+        getWeatherByCity(insertWeatherData, showError, $('#search-loc').val());
+    });
+    
+    getWeatherData(insertWeatherData, showError);
+    
+     $('#search-loc').keypress(function(e) {
+        var ENTER_KEY_CODE = 13;
+        if ( e.which === ENTER_KEY_CODE ) 
+        {
+            $('#btn').trigger('click');
+            return false;
+        }
+    }); 
+    
+    
+    
+    
+    
     var dayNum = 0;
     //var currentGeolocation;
 
@@ -9,9 +28,9 @@ $( document ).ready(function() {
     navigationSlider(dayNum);
      
      
-    if ($('body').hasClass('home')) {
+    /*if ($('body').hasClass('home')) {
         navigator.geolocation.getCurrentPosition(getWeatherData, error);
-    } 
+    } */
      
      
     // function positionLatLon(position, currentGeolocation) {
@@ -60,6 +79,9 @@ $( document ).ready(function() {
     });
 
 
+    function showError(msg){
+        $('.forecast-box').html('<div class="geolocationerror">' + msg + '</div>');
+	}
 });
 
 
@@ -76,26 +98,26 @@ function navigationSlider(dayNum) {
 
 
 
-function getWeatherData(position){
+/*function getWeatherData(position){
     $.getJSON('http://api.openweathermap.org/data/2.5/forecast/daily?lat=' + position.coords.latitude + '&lon=' +position.coords.longitude + '&units=metric' + '&lang=en&callback=?&appid=2239135b18e8b5e093a144e55b94b5d6', function(data){
         $('.button-box').addClass('visible');
         insertWeatherData(data);
         charts(data);
     	
     });
-}
+}*/
 
-function error(err) {
+/*function error(err) {
   $('.forecast-box').html('<div class="geolocationerror">Please choose location for weather forecast</div>');
   console.warn('ERROR(' + err.code + '): ' + err.message);
-}
+}*/
 
 function insertWeatherData(data){
     var offset = (new Date()).getTimezoneOffset()*60*1000;
     	
 	var localTime = new Date( data.list[0].dt*1000 - offset);
 	var currentDay = moment(localTime).calendar();
-	var currentDate = moment(localTime).format('MMMM D');
+	var currentDate = moment(localTime).format('MMMM, D');
 	
 	var localTimeSecondDay = new Date( data.list[0+1].dt*1000 - offset);
 	var secondDay = moment(localTimeSecondDay).calendar();
@@ -150,6 +172,7 @@ function insertWeatherData(data){
                     break;
             }
     //insert weather data in main box
+    $('.button-box').addClass('visible');
     $('#forecast-date').html('<div class="day-active">' + currentDay + '</div>' +
                             '<div class="data-active">' + currentDate + '</div>');
     $('#forecast-img').html('<img src="img/weather-img/' + data.list[0].weather[0].icon + '.png" alt="weather icon" />' + 
@@ -163,8 +186,103 @@ function insertWeatherData(data){
     $('.sidebar-forecast-close.third-day').html('<img src="img/weather-img/white/' + data.list[0+2].weather[0].icon + '.png"> <div class="side-data-sm">' + thirdDateSmall + '</div>');
     $('.sidebar-forecast-open.forth-day').html('<div class="side-temp">' + Math.round(data.list[0+3].temp.day) + '&deg</div> <div class="side-forecast-img"> <img src="img/weather-img/white/' + data.list[0+3].weather[0].icon + '.png"> </div> <p class="side-day">' + fourthDay + '</p>');
     $('.sidebar-forecast-close.forth-day').html('<img src="img/weather-img/white/' + data.list[0+3].weather[0].icon + '.png"> <div class="side-data-sm">' + fourthDateSmall + '</div>');
+    
+    // insert weather data in chart boxes
+    var tempChartData = {
+    	labels : ['Morning','Daytime','Evening','Night'],
+    	datasets : [
+    		{
+    			label: 'Chart whith temperature',
+    			fillColor : 'white',
+    			strokeColor : '#0A94C2',
+    			pointColor : '#991C1F',
+    			pointStrokeColor : 'white',
+    			pointHighlightFill : '#EF292B',
+    			pointHighlightStroke : 'rgba(255,255,255,0.5)',
+    			data : [Math.round(data.list[0].temp.morn),
+    			        Math.round(data.list[0].temp.day),
+    			        Math.round(data.list[0].temp.eve),
+    			        Math.round(data.list[0].temp.night)],
+    		}
+    	]
+    
+    }
+    var optionsTemp = 
+    {
+        tooltipTemplate: '<%= value %>°',
+        showTooltips: true,
+        onAnimationComplete: function()
+        {    
+            this.showTooltip(this.datasets[0].points, true);          
+        },
+        tooltipEvents: []
+    };
+    var chartTemp = document.getElementById('chart-temp').getContext('2d');
+    new Chart(chartTemp).Line(tempChartData, optionsTemp, {
+    	responsive: true
+    });
+
+    var humidityData = [
+		{
+			value: data.list[0].humidity,
+			color:'#0A94C2',
+			highlight: '#0A94C2',
+			label: ''
+		},
+		{
+			value: 100-data.list[0].humidity,
+			color: 'white',
+			highlight: 'white',
+			label: ''
+		},
+	];
+    var optionsHumidity = 
+        {
+            tooltipTemplate: '<%= value %>%',
+            showTooltips: true,
+            onAnimationComplete: function()
+            {    
+                this.showTooltip(this.segments, true);          
+            },
+            tooltipEvents: []
+        };
+        
+	var chartHumidity = document.getElementById('chart-humidity').getContext('2d');
+    new Chart(chartHumidity).Doughnut(humidityData, optionsHumidity, {
+		responsive: true
+	});
+	
+	var pressureData = {
+		labels : ['Normal','Current'],
+		datasets : [
+			{
+				fillColor : '#0A94C2',
+				strokeColor : 'white',
+				highlightFill: '#991C1F',
+				highlightStroke: 'white',
+				data : [760, Math.round(data.list[0].pressure*0.7501)] //convert from hPa to mm Hg
+			},
+		]
+	};
+    var optionsPressure = 
+        {
+            showTooltips: true,
+            onAnimationComplete: function()
+            {    
+                this.showTooltip(this.datasets[0], true);          
+            },
+            tooltipEvents: []
+        };
+        
+	var chartPressure = document.getElementById('chart-pressure').getContext('2d');
+    new Chart(chartPressure).Bar(pressureData, //optionsPressure, 
+                                {responsive: true
+	});
+    
+    
 }
 
+/* inserted this function in main function
 function charts (data){
       var tempChartData = {
     			labels : ['Morning','Daytime','Evening','Night'],
@@ -258,3 +376,4 @@ function charts (data){
     	                                {responsive: true
     		});
 }
+*/
---
+++ js/weather-services.js
@@ -0,0 +1,41 @@
+function getWeatherData(fnOK, fnError){
+    if ($('body').hasClass('home')) {
+        navigator.geolocation.getCurrentPosition(locSuccess, locError);
+    } 
+    function locSuccess(position) {
+        $.getJSON('http://api.openweathermap.org/data/2.5/forecast/daily?lat=' + position.coords.latitude + '&lon=' +position.coords.longitude + '&units=metric&lang=en&callback=?&appid=2239135b18e8b5e093a144e55b94b5d6', 
+            function(data){
+        	    fnOK.call(this, data);
+            });
+    }
+    
+    function locError(error) {
+        var message = 'Location error. ';
+        switch(error.code) {
+            case error.TIMEOUT:
+                message += 'A timeout occured! Please try again!';
+                break;
+            case error.POSITION_UNAVAILABLE:
+                message += 'We can\'t detect your location. Sorry!';
+                break;
+            case error.PERMISSION_DENIED:
+                message += 'Please allow geolocation access for this to work.';
+                break;
+            case error.UNKNOWN_ERROR:
+                message += 'An unknown error occured!';
+                break;
+        }
+        fnError.call(this, message);
+        }
+}
+
+
+function getWeatherByCity(fnOK, fnError, cityName) {
+    $.getJSON(
+        'http://api.openweathermap.org/data/2.5/forecast/daily?q=' 
+        + cityName + '&cnt=16&units=metric' + '&lang=en&callback=?&appid=2239135b18e8b5e093a144e55b94b5d6',
+        function (data) {
+            fnOK.call(this, data);
+        }
+    );
+}
